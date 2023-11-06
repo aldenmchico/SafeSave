8acf73232f837b4793c8f71e4ab8a4e5eafb9221
justpolishedmygrapple <mangela@oregonstate.edu>
2023-11-06 01:48:57 -0800

    working! just need the other fields.

--- application-microservices/data-decryption-microservice/data-decryption-model.mjs
+++ application-microservices/data-decryption-microservice/data-decryption-model.mjs
@@ -18,6 +18,54 @@ const getUserNoteIV = async (noteIVQuery) => {
     });
 };
 
+const getNoteTitle = async (noteTitleQuery) => {
+    return new Promise((resolve, reject) => {
+        db.pool.query(noteTitleQuery, function(error, iv, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                if (iv[0] && iv[0].userNoteTitle) {
+                    resolve(iv[0].userNoteTitle);
+                } else {
+                    reject(new Error('userNoteIV not found.'));
+                }
+            }
+        });
+    });
+};
+
+const getNoteText = async (noteTextQuery) => {
+    return new Promise((resolve, reject) => {
+        db.pool.query(noteTextQuery, function(error, iv, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                if (iv[0] && iv[0].userNoteText) {
+                    resolve(iv[0].userNoteText);
+                } else {
+                    reject(new Error('userNoteIV not found.'));
+                }
+            }
+        });
+    });
+};
+
+const getNoteCreated = async (noteCreatedQuery) => {
+    return new Promise((resolve, reject) => {
+        db.pool.query(noteCreatedQuery, function(error, iv, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                if (iv[0] && iv[0].userNoteCreated) {
+                    resolve(iv[0].userNoteCreated);
+                } else {
+                    reject(new Error('userNoteIV not found.'));
+                }
+            }
+        });
+    });
+};
+
 const getDecryptedData = async (userNoteID, userHash) => {
     try {
         if (!userNoteID || !userHash ) {
@@ -27,9 +75,18 @@ const getDecryptedData = async (userNoteID, userHash) => {
 
         const userNoteIDBuffer = Buffer.from(userNoteID, 'utf8')
 
+        //SQL queries
         const noteIVQuery = `SELECT userNoteIV FROM UserNotes where UserNoteID = '${userNoteIDBuffer}'`;
+        const noteTitleQuery = `SELECT userNoteTitle FROM UserNotes where UserNoteID = '${userNoteIDBuffer}'`;
+        const noteTextQuery = `SELECT userNoteText FROM UserNotes where UserNoteID = '${userNoteIDBuffer}'`;
+        const userNoteCreatedQuery = `SELECT userNoteCreated FROM UserNotes where UserNoteID = '${userNoteIDBuffer}'`;
+        const userNoteAccessedQuery = `SELECT userNoteCreated FROM UserNotes where UserNoteID = '${userNoteIDBuffer}'`;
+
 
         const userNoteIV = await getUserNoteIV(noteIVQuery);
+        const userNoteTitle = await getNoteTitle(noteTitleQuery);
+        const userNoteText = await getNoteText(noteTextQuery);
+        const userNoteCreated = await getNoteCreated(userNoteCreatedQuery);
 
         const userNoteIVBuffer = Buffer.from(userNoteIV, 'hex');
 
@@ -37,19 +94,25 @@ const getDecryptedData = async (userNoteID, userHash) => {
 
         console.log(userKey)
 
-        // Convert the key and IV from hexadecimal to binary
-        // const key = Buffer.from(keytext, 'hex');
-        // const iv = Buffer.from(userNoteIV, 'hex');
+        const decipherTitle = crypto.createDecipheriv('aes-256-cbc', userKey, userNoteIVBuffer);
+        const decipherText = crypto.createDecipheriv('aes-256-cbc', userKey, userNoteIVBuffer);
+        const decipherCreated = crypto.createDecipheriv('aes-256-cbc', userKey, userNoteIVBuffer);
 
-        const decipher = crypto.createDecipheriv('aes-256-cbc', userKey, userNoteIVBuffer);
+        let decryptedNoteTitle = decipherTitle.update(userNoteTitle, 'hex', 'utf8');
+        decryptedNoteTitle += decipherTitle.final('utf8');
 
-        let decryptedData = decipher.update("1cdbe8073860187ae22b3fa49a5f23e8", 'hex', 'utf8');
-        decryptedData += decipher.final('utf8');
+        let decryptedNoteText = decipherText.update(userNoteText, 'hex', 'utf8');
+        decryptedNoteText += decipherText.final('utf8');
 
-        console.log('Decrypted Data:', decryptedData);
+        let decryptedNoteCreated = decipherCreated.update(userNoteCreated, 'hex', 'utf8');
+        decryptedNoteCreated += decipherCreated.final('utf8')
+
+        console.log('Decrypted Data:', decryptedNoteTitle);
 
         return {
-            decryptedData
+            decryptedNoteTitle: decryptedNoteTitle,
+            decryptedNoteText: decryptedNoteText,
+            decryptedNoteCreated: decryptedNoteCreated
         };
     } catch (error) {
         console.error('Decryption failed:', error);
