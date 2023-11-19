9fdb6cd5073f016595743d2796e3a5919e6d69a2
justpolishedmygrapple <mangela@oregonstate.edu>
2023-11-19 03:55:09 -0800

    Cookie now has encrypted session ID

--- application-frontend/src/components/SavedLoginList.js
+++ application-frontend/src/components/SavedLoginList.js
@@ -4,31 +4,39 @@ import SavedLoginRow from './SavedLoginRow';
 
 
 function SavedLoginList({loginItems, editLoginItem, deleteLoginItem}) {
-    return (
-        <div className="wrapper">
-        <table>
-            <thead>
-                <tr>
-                    <th>Website</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Date Created</th>
-                    <th>Date Updated</th>
-                    <th>Date Accessed</th>
-                </tr>
-            </thead>
-            <tbody>
-                {loginItems.map((loginItem, i) =>
-                <SavedLoginRow
-                    loginItem={loginItem}
-                    key={i}
-                    editLoginItem={editLoginItem}
-                    deleteLoginItem={deleteLoginItem}
-                />)}
-            </tbody>
-        </table>
-        </div>
-    );
+
+    if (loginItems.length !== 0 ) {
+        return (
+            <div className="wrapper">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Website</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Date Created</th>
+                        <th>Date Updated</th>
+                        <th>Date Accessed</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {loginItems.map((loginItem, i) =>
+                        <SavedLoginRow
+                            loginItem={loginItem}
+                            key={i}
+                            editLoginItem={editLoginItem}
+                            deleteLoginItem={deleteLoginItem}
+                        />)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    else {
+        return(
+            <p></p>
+        )
+    }
 }
 
 export default SavedLoginList;
--- application-microservices/jwt-microservice/jwt-model.mjs
+++ application-microservices/jwt-microservice/jwt-model.mjs
@@ -1,22 +1,48 @@
 
 import jwt from 'jsonwebtoken'
 import 'dotenv/config';
+import crypto from 'crypto';
+import mysql from 'mysql';
+import * as db from "./db-connector.mjs";
+
 
 // const SECRET_KEY = process.env.SECRET;
 
+const con = mysql.createConnection(db.dbConfig);
+
+//TODO
 const SECRET = 'ChangeLater';
 
+
 const signJwtToken = (user) => {
+    try{
+        const sessionID = crypto.randomBytes(16).toString('hex');
+        const userUsername = user.userUsername;
+        const userID = user.userID;
 
-    const { userUsername, userID, user2FAEnabled } = user;
-    const token = jwt.sign({
-        userUsername,
-        userID
-    }, SECRET, { expiresIn: '3hr' }); // Adjust expiresIn as needed
-    console.log(`token created at signJwtToken ${token}`);
+        const hmac = crypto.createHmac('sha256', SECRET)
+            .update(userUsername + userID + sessionID)
+            .digest('hex');
 
-    return token
-}
+        const query = 'UPDATE Users SET userSessionID = ? WHERE userID = ?';
+        con.query(query, [hmac, userID]);
+
+        console.log('Query executed successfully');
+
+        const token = jwt.sign({
+            userUsername,
+            userID,
+            sessionID
+        }, SECRET, { expiresIn: '3hr' });
+
+        console.log(`Token created at signJwtToken: ${token}`);
+        return token;
+    } catch (error) {
+        console.error('Error in signJwtToken:', error);
+        throw error;
+    }
+};
+
 
 // Exports for jwt microservice-controller
 export { signJwtToken };
