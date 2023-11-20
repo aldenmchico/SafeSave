5f11ce339595cd6f7ff5b0e8525dce065a97083a
justpolishedmygrapple <mangela@oregonstate.edu>
2023-11-20 13:19:49 -0800

    database connector files

---
+++ application-microservices/jwt-microservice/db-connector.mjs
@@ -0,0 +1,9 @@
+let dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'DemoDatabasePassword!!123',
+    database: 'capstone_2023_securepass1',
+    multipleStatements: true
+}
+
+export {dbConfig}
