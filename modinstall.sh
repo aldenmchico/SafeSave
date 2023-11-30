e516a60a9cd501d66e7507e740f88cd51c5c28f6
justpolishedmygrapple <mangela@oregonstate.edu>
2023-11-29 21:07:08 -0800

    removing old db's

--- modinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find . -name 'package.json' -not -path '*node_modules*' | while read packageFile; do
-    dir=$(dirname "$packageFile")
-    echo "Installing npm packages in $dir"
-    (cd "$dir" && npm install)
-done
